990b72411e1aad7859882d92366e5e884e5d66ab
T.Kav <tomas@plienas.lt>
2022-07-13 01:49:38 +0300

    optimizuotas kodas sumazinant eiluciu

--- src/components/Li.js
+++ src/components/Li.js
@@ -1,27 +1,14 @@
 import { useState } from "react"
 import ModalProfile from "./ModalProfile"
 
-
 export default function Li(props) {
     const { index, list, setList, userId, email, nameShort, name, lname, age, setName } = props
     const [buttonClicked, setButtonClicked] = useState(false)
     const [editButtonClicked, setEditButtonClicked] = useState(false)
     const [isPending, setIsPending] = useState(false)
   
-    function showMore() {
-        if (!buttonClicked) {
-            setButtonClicked(true)
-        }
-        else { setButtonClicked(false) }
-    }
-
-    function modalOn() {
-        if (!editButtonClicked) {
-            setEditButtonClicked(true)
-        }
-        else { setEditButtonClicked(false) }
-    }
-
+    const showMore=()=>!buttonClicked? setButtonClicked(true): setButtonClicked(false)
+    const modalOn=()=> !editButtonClicked?setEditButtonClicked(true): setEditButtonClicked(false)
 
     function handleDelete(id) {
         setIsPending(true)
@@ -44,70 +31,21 @@ export default function Li(props) {
             .catch((error) => {
                 alert(error)
             });
-
     }
-
-    // function handleDelete(id) {
-    //     fetch(`http://127.0.0.1:9000/users/${id}`, {
-    //         method: 'DELETE'
-    //     }).then((result) => {
-    //         result.json()
-    //             .then((resp) => {
-    //                 console.warn(resp)
-    //             })
-    //             .catch((error) => {
-    //                 console.log(error)
-    //             })
-
-    //     })
-
-    // }
-
-    // async function onDelete() {
-
-    //     alert("veikia")
-    // await fetch(`http://127.0.0.1:9000/users/:${id}`, {
-    //     method: "DELETE",
-    // })
-    //     .then((res) => {
-    //         if (res.status !== 200) {
-    //             return alert('klaida');
-    //         } else {
-    //             const newList = list
-    //             newList.splice(index, 1)
-    //             setList([...newList])
-
-
-    //         }
-    //     })
-    //     .catch((err) => {
-    //         console.log(err);
-    //     });
-    // };
-
-    // function closeModal() {
-    //     return !editButtonClicked
-    // }
-
 
     return (
         <li key={index}>
-            {!buttonClicked && <div className="showMore"> {nameShort}. {lname}</div>}
-            {buttonClicked && <div className="showMore">  {name} {lname}, {email}, amzius {age}m.</div>}
+            {!buttonClicked?<div className="showMore"> {nameShort}. {lname}</div>:
+            <div className="showMore">  {name} {lname}, {email}, amzius {age}m.</div>}
+
             <div className="buttonHolder">
-                {!isPending && <button onClick={showMore}>
-                    {buttonClicked ? "Mažiau" : "Daugiau"}
-                </button>}
-                {isPending && ""}
-
-
+                {!isPending? <button onClick={showMore}> {buttonClicked ? "Mažiau" : "Daugiau"}</button>:
+                isPending && ""}
 
                 {!editButtonClicked && <button style={{ marginTop: "10px" }} onClick={modalOn}> Redaguoti dalyvio duomenis </button>}
 
-                {!isPending && <button style={{ marginTop: "10px" }} onClick={() => handleDelete(userId)}>Istrinti dalyvi</button>}
-                {isPending && <button style={{ marginTop: "10px" }} onClick={() => handleDelete(userId)} disabled> Dalyvis trinamas... </button>}
-
-
+                {!isPending ? <button style={{ marginTop: "10px" }} onClick={() => handleDelete(userId)}>Istrinti dalyvi</button>:
+                <button style={{ marginTop: "10px" }} disabled> Dalyvis trinamas... </button>}
 
                 {editButtonClicked && <ModalProfile
                     click={() => setEditButtonClicked(false)}
@@ -121,11 +59,7 @@ export default function Li(props) {
                     email={email}
                     age={age}
                     setList={setList} />
-
                 }</div>
-
-
-
         </li>
     )
 }
